541bcb2184849dd045e2d90439b033abcb82305b
bigcupcoffee <bigcupcoffee@users.noreply.github.com>
2019-11-06 08:36:11 +0200

    Ended the t5

--- t5.cpp
+++ t5.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+#define M 3
+#define N 5
+
 int main()
 {
-  int *a = new int[5];
+  int *a[M], max[M];
+  for (int i = 0; i < M; i++)
+  {
+    a[i] = new int[N];
+    max[i] = 0;
+    for (int j = 0; j < N; j++)
+    {
+      cout << "arr[" << i << "][" << j << "] = ";
+      cin >> a[i][j];
+      if (a[i][j] > max[i])
+        max[i] = a[i][j];
+    }
+    cout << "max[" << i << "] is " << max[i] << endl;
+  }
 
   system("pause");
   return 0;
